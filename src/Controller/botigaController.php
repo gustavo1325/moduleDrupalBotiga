e72195c0a46e3a758e249489444199b865a204ba
gustavo1325 <87393323+gustavo1325@users.noreply.github.com>
2022-04-21 11:52:39 +0200

    Update botigaController.php

--- src/Controller/botigaController.php
+++ src/Controller/botigaController.php
@@ -13,11 +13,6 @@ class botigaController extends ControllerBase {
   public function template() {
     return [
       '#theme' => 'botiga',
-      '#tlf1' => $this->t('645223365'),
-      '#tlf2' => $this->t('635774410'),
-
-    //'#type' => 'markup',
-      //'#markup' => $this->t('Hola caracola'),
     ];
   }
 
